55ecd189944395ae27d1a6b24c8321658a215546
Brian Kipkosgei <brianktoo1998@gmail.com>
2024-04-15 23:21:56 +0300

    ternary operator examples

---
+++ ternary_operator.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+let result = (20 > 0) ? true:false;
+console.log(result)
+
+let message = (20>19) ? "yes": "no";
+console.log(message)
+
+let marks=39
+let pMark=(marks > 40) ? "pass": "fail";
+console.log(pMark)
